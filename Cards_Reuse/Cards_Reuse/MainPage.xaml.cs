2614429fc11b55bd5ab282e3f0f66be367899335
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-05-26 12:17:29 +0530

    Sample Included

---
+++ Cards_Reuse/Cards_Reuse/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Syncfusion.XForms.Cards;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace Cards_Reuse
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        int i = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+            On<iOS>().SetUseSafeArea(true);
+        }
+
+        private async void SfCardView_Dismissed(System.Object sender, Syncfusion.XForms.Cards.DismissedEventArgs e)
+        {
+            await Task.Delay(400);
+            i++;
+            SfCardView view = sender as SfCardView;
+            ((view.Content as StackLayout).Children[0] as Label).Text = "CardView " + i.ToString();
+            view.IsDismissed = false;
+        }
+
+    }
+}
